44132ecb7414478ff88893c1ee0f8b4da5bb0fb4
Alex Qiang <alex.qiang36@gmail.com>
2020-12-12 18:20:06 +1100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Projects from './Projects.js'; 
+import About from './About.js'
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+  
+        {/* Set up the Router */}
+        <Route exact path="/" component={About} />
+        <Route path="/projects" component={Projects} />
+        
+  
+        <div className="nav">
+          {/* <img src={logo} className="logo-image" alt="Logo Image" /> */}
+          <div className="nav-sub">
+                                          
+            {/* Set up the Links */}
+            <Link to="/" className="item">About</Link>
+            <Link to="/projects" className="item">Projects</Link>
+            <a href="https://github.com/AlexQ6" className="item">GitHub</a>
+            <a href="https://linkedin.com/in/alex-qiang" className="item">LinkedIn</a>
+            
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );  
 }
-
 export default App;
