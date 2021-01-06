47213b88cdc9d5a2f3ef6c113c2c2b86247dc1fc
Alex Qiang <alex.qiang36@gmail.com>
2021-01-06 22:36:09 +1100

    Replace links with icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { GoMarkGithub } from "react-icons/go";
+import { FaLinkedinIn } from "react-icons/fa"
 
+    
 function Footer() {
 
     return(
@@ -14,11 +17,11 @@ function Footer() {
                     </Col>
 
                     <Col className="p-0  text-center" >
-                        <a href="https://github.com/AlexQ6" target="_blank" rel="noopener noreferrer">GitHub</a>   
+                        <a href="https://github.com/AlexQ6" target="_blank" rel="noopener noreferrer"><GoMarkGithub size="23"/></a> 
                     </Col> 
 
                     <Col className="p-0 d-flex justify-content-end" >
-                        <a href="https://www.linkedin.com/in/alex-qiang/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+                        <a href="https://www.linkedin.com/in/alex-qiang/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn size="23"/></a> 
                     </Col>
 
                 </Row>
